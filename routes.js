c577dc78ba39cae5c4eaabb26b3d48f11ed845b9
Philip McNamara <philipmcnamara1@gmail.com>
2022-01-03 20:04:44 +0000

    basic screens added

--- routes.js
+++ routes.js
@@ -2,13 +2,15 @@ const PS = require('./app/controllers/ProblemSolver');
 
 module.exports = [
   { method: 'GET', path: '/', config: PS.index },
+  { method: "GET", path: "/signup", config: PS.signup },
+  { method: "GET", path: "/login", config: PS.login },
   {
-      method: 'GET',
-      path: '/{param*}',
-      handler: {
-        directory: {
-          path: './public',
-        },
+    method: 'GET',
+    path: '/{param*}',
+    handler: {
+      directory: {
+        path: './public',
       },
     },
-  ];
+  },
+];
