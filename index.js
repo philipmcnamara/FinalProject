ee1ea36d7c30517e83d4d306c8614962d52e6fc0
Philip McNamara <philipmcnamara1@gmail.com>
2022-01-06 16:59:18 +0000

    hapi joi configured

--- index.js
+++ index.js
@@ -1,33 +1,39 @@
-'use strict';
+"use strict";
 
-const Hapi = require('@hapi/hapi');
-const Inert = require('@hapi/inert');
-const Vision = require('@hapi/vision');
-const Handlebars = require('handlebars');
+const Hapi = require("@hapi/hapi");
+const Inert = require("@hapi/inert");
+const Vision = require("@hapi/vision");
+const Handlebars = require("handlebars");
 const Cookie = require("@hapi/cookie");
-require('./app/models/db');
-const env = require('dotenv');
+const Joi = require("@hapi/joi");
+require("./app/models/db");
+const env = require("dotenv");
 
+const dotenv = require("dotenv");
 
-env.config();
+const result = dotenv.config();
+if (result.error) {
+  console.log(result.error.message);
+  process.exit(1);
+}
 
 const server = Hapi.server({
-  port: 3000,
-  host: 'localhost',
+  port: process.env.PORT || 3000,
 });
 
 async function init() {
   await server.register(Inert);
   await server.register(Vision);
   await server.register(Cookie);
+  server.validator(require("@hapi/joi"));
   server.views({
     engines: {
-      hbs: require('handlebars'),
+      hbs: require("handlebars"),
     },
     relativeTo: __dirname,
-    path: './app/views',
-    layoutPath: './app/views/layouts',
-    partialsPath: './app/views/partials',
+    path: "./app/views",
+    layoutPath: "./app/views/layouts",
+    partialsPath: "./app/views/partials",
     layout: true,
     isCached: false,
   });
@@ -35,22 +41,19 @@ async function init() {
     cookie: {
       name: process.env.cookie_name,
       password: process.env.cookie_password,
-      isSecure: false
+      isSecure: false,
     },
     redirectTo: "/",
   });
-
-  server.auth.default('session');
-  server.route(require('./routes'));
+  server.auth.default("session");
+  server.route(require("./routes"));
   await server.start();
   console.log(`Server running at: ${server.info.uri}`);
 }
 
-process.on('unhandledRejection', (err) => {
+process.on("unhandledRejection", (err) => {
   console.log(err);
   process.exit(1);
 });
-
-
 
 init();
