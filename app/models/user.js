59fb1aaefe3f97ab2d2eda8f90cda4df32446432
Philip McNamara <philipmcnamara1@gmail.com>
2022-02-26 13:14:05 +0000

    deploy test

--- app/models/user.js
+++ app/models/user.js
@@ -3,7 +3,7 @@
 const Mongoose = require("mongoose");
 const Boom = require("@hapi/boom");
 const Schema = Mongoose.Schema;
-//const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt');
 
 const userSchema = new Schema({
   firstName: String,
